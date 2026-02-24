2d8a2517c8bf0c813c00a34fc86c0d6c5ad02b6d
Hassan Alkhalaf <baraanas1521@gmail.com>
2026-02-24 17:52:27 +0300

    tsx => css

--- src/componints/LoadAi.tsx
+++ src/componints/LoadAi.tsx
@@ -1,25 +1,17 @@
-import { useShoppingcart } from "./context/useShoppingcart";
 
 function LoadAi() {
-   
   return (
     <div className="perant-LI">
-    { Array.from(new Array(3)).map(() => (
-
+      {Array.from(new Array(3)).map(() => (
         <div className="perant_load">
-            <div className="imag_load">
-    
-            </div>
+          <div className="imag_load"></div>
           <div className="perant_load_message">
-            {Array.from(new Array(3)).map((_,index) => (
-                <span key={index} className="loading_ai_message"></span>
+            {Array.from(new Array(3)).map((_, index) => (
+              <span key={index} className="loading_ai_message"></span>
             ))}
           </div>
         </div>
-    ))
-
-    }
-    
+      ))}
     </div>
   );
 }
--- src/componints/Shoppingcart.tsx
+++ src/componints/Shoppingcart.tsx
@@ -1,46 +1,18 @@
 import { RiShoppingBagFill } from "@remixicon/react";
 import { useShoppingcart } from "./context/useShoppingcart";
-
+import "./css/Shoppingcart.css";
 
 function Shoppingcart() {
-  const {count} = useShoppingcart()
+  const { count } = useShoppingcart();
   return (
-    <div
-      style={{
-        width: "100%",
-        height: "60px",
-        boxShadow: "",
-        borderBottom: "1px solid #1565c023",
-        
-      }}>
-      <div style={{position: "relative",}}>
-        <button
-          style={{
-            position: "absolute",
-            right: 300,
-            backgroundColor: "#1565c023",
-          }}>
+    <div className="Shoppingcart_box">
+      <div className="Shoppingcart_relative_container">
+        <button className="Shoppingcart_button">
           <RiShoppingBagFill color="#1565c0" />
         </button>
 
         {count.length > 0 && (
-          <div
-            style={{
-              position: "absolute",
-              top: "-1px",
-              right: 300,
-              backgroundColor: "#ff4d4f", // اللون الأحمر للتنبيه
-              color: "white",
-              borderRadius: "50%",
-              padding: "2px 6px",
-              fontSize: "10px",
-              fontWeight: "bold",
-              minWidth: "8px",
-              textAlign: "center",
-              // border: "2px solid #1a222d", // ليعطي فصلاً بين الأيقونة والرقم
-            }}>
-            {count.length}
-          </div>
+          <div className="Shoppingcart_count">{count.length}</div>
         )}
       </div>
     </div>
