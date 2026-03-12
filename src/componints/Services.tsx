127d7f425caf88b95b1af1fb3deb2be70eb1ed58
Hassan Alkhalaf <baraanas1521@gmail.com>
2026-03-12 06:39:33 +0300

    add new sc

---
+++ src/componints/Services.tsx
@@ -0,0 +1,12 @@
+import Box from "./Nodos/Box"
+
+function Services() {
+  return (
+    <div className="services" id="services">
+        <Box more="" text=""/>
+      
+    </div>
+  )
+}
+
+export default Services
