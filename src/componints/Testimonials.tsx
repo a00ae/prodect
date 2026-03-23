123be8aff2c06ce5f7ca526f8a0edf4edd4ef013
Hassan Alkhalaf <baraanas1521@gmail.com>
2026-03-23 15:29:52 +0300

    add new clients

--- src/componints/Testimonials.tsx
+++ src/componints/Testimonials.tsx
@@ -1,17 +1,52 @@
 import Box from "./Nodos/Box";
 import { useBoxData } from "./context/BoxProvider";
-import "./css/Testimonials.scss"
+import "./css/Testimonials.scss";
+
+const cardImges = Array.from(
+  { length: 4 },
+  (_, i) => `/Testimonials/testimonials-0${i + 1}.avif`,
+);
+
 
 function Testimonials() {
-    const {testimonials} = useBoxData();
+  const { testimonials } = useBoxData();
   return (
     <div className="testimonials" id="testimonials">
-        <div className="container">
-            <Box title={testimonials.title} more={testimonials.more} />
+      <div className="container">
+        <Box title={testimonials.title} more={testimonials.more} />
+
+        <div className="testimonials_card">
+          <div className="testimonials_card_item_1">
+            <p>
+              We’ve had the privilege of partnering with ambitious brands
+              worldwide. Here’s how we helped them succeed.
+            </p>
+            <div className="testimonials_descrption">
+
+            <div className="image_container">
+              <div>
+
+              {cardImges.map((ele, index) => (
+                <div key={index} className="testimoniles_image">
+                  <img  src={ele} alt="" />
+
+                </div>
+              ))}
+              <div className="testimoniles_image">
+                <p>95+</p>
+              </div>
+              </div>
+
+              <div className="testimoniles_text-rate">
+                
+              </div>
+            </div>
+            </div>
+          </div>
         </div>
-      
+      </div>
     </div>
-  )
+  );
 }
 
-export default Testimonials
+export default Testimonials;
