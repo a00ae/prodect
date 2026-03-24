20fde6ba1fb9eb45f513a5fcdeea62eec5606a7d
Hassan Alkhalaf <baraanas1521@gmail.com>
2026-03-25 00:38:46 +0300

    add new clients

--- src/componints/Nodos/ScTitle.tsx
+++ src/componints/Nodos/ScTitle.tsx
@@ -1,16 +1,17 @@
-import "../css/studio.scss"
+import "../css/studio.scss";
 import type { Shopping } from "../opp/opp";
-function ScTitle({data, title, projects,}: Shopping) {
+function ScTitle({ data, title, projects }: Shopping) {
   return (
     <div className="title">
-            <div>
-              <p>{data}</p>
-              <p>|</p>
-              <p>{title}</p>
-            </div>
-            <p>{projects}</p>
+      <div>
+        <p>{data}</p>
+        <p>|</p>
+        <p>{title}</p>
+      </div>
+
+      {projects && <p>{projects}</p>}
     </div>
-  )
+  );
 }
 
-export default ScTitle
+export default ScTitle;
