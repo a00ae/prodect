347e7f7296e8efc01cdf207159ec95ac43cc91c4
Hassan Alkhalaf <baraanas1521@gmail.com>
2026-03-26 22:19:14 +0300

    add new data

--- src/componints/CaseStudy.tsx
+++ src/componints/CaseStudy.tsx
@@ -12,11 +12,16 @@ const CaseStudy = (props: Props) => {
       <div className={styles.container}>
         <Box title={caseStudy.title} more={caseStudy.more} />
 
+
+        <div className={styles.card}>
+
         <div className={styles["card-item"]}>
           {cardCaseStudy
             .slice(0, 3)
             .map(({ id, desc, title, img, isActive }) => (
-              <div key={id} className={styles["box-container"]}>
+              <div
+                key={id}
+                className={`${styles["box-container"]} ${isActive == "img1" ? styles[isActive] : ""}`}>
                 <div className={styles.top}>
                   <p>{title}</p>
                 </div>
@@ -27,8 +32,8 @@ const CaseStudy = (props: Props) => {
                         <img src={img} alt="" />
                       </div>
                       <div className={styles["text-desc"]}>
-                        <p>{desc[0]}</p>
-                        <p>{desc[1]}</p>
+                        <p>{Array.isArray(desc) ? desc[0] : ""}</p>
+                        <p>{Array.isArray(desc) ? desc[1] : ""}</p>
                       </div>
                     </>
                   ) : (
@@ -38,6 +43,22 @@ const CaseStudy = (props: Props) => {
               </div>
             ))}
         </div>
+        <div className={styles["global-card"]}>
+            {cardCaseStudy.slice(3,).map((prev) => (
+                <div key={prev.id} className={styles["card-sm"]}>
+                    <div className={styles.top}>
+                        <p>{prev.title}</p>
+                        <span>{prev.id == 6 ? "+" : "%"}</span>
+                    </div>
+                    <div className={styles.bottom}>
+                        <p>{prev.desc}</p>
+                    </div>
+                </div>
+            ))}
+        </div>
+
+        </div>
+
       </div>
     </div>
   );
