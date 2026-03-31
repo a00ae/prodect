005937e79ca4b69486dfe185a3d0e25181277f59
Hassan Alkhalaf <baraanas1521@gmail.com>
2026-03-31 22:29:32 +0300

    add new componints name Blog

--- src/componints/Blog.tsx
+++ src/componints/Blog.tsx
@@ -10,6 +10,24 @@ const Blog = () => {
     <div className={styles.blog}>
       <div className={styles.container}>
         <Box {...blog} />
+
+        <div className={styles["card-container"]}>
+          <a href="#">
+            <div className={styles["text-card"]}>
+              <div className={styles.top}>
+                <span>Oct 09, 2025</span>
+                <h3>How Visual Identity S</h3>
+              </div>
+              <div className={styles.bottom}>
+                <p>
+                  A deep dive into how thoughtful visual systems influence
+                  customer trust, from color psychology to logo simplicity.
+                </p>
+              </div>
+            </div>
+            <div className={styles.image}>{/* <img src="/" alt="" /> */}</div>
+          </a>
+        </div>
       </div>
     </div>
   );
