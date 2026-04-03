bd6e0072981912c8eab9bf0f3bdd9a75714e8f9c
Hassan Alkhalaf <baraanas1521@gmail.com>
2026-04-03 17:19:45 +0300

    add new typescript componints dev

--- src/componints/Newsletter.tsx
+++ src/componints/Newsletter.tsx
@@ -1,26 +1,23 @@
-import { memo } from 'react'
-import styles from "./css/Newsletter.module.scss"
-import Box from './Nodos/Box';
-import { useBoxData } from './context/BoxProvider';
-import { BoxSection } from './opp/opp';
-import Text from './Nodos/Text';
-type Props = {}
+import { memo } from "react";
+import styles from "./css/Newsletter.module.scss";
+import Box from "./Nodos/Box";
+import { useBoxData } from "./context/BoxProvider";
+import { BoxSection } from "./opp/opp";
+import Text from "./Nodos/Text";
 
-function Newsletter({}: Props) {
-    const newsletter = useBoxData(BoxSection.Newsletter)
+
+function Newsletter() {
+  const newsletter = useBoxData(BoxSection.Newsletter);
   return (
     <div className={styles["newsletter"]}>
-        <div className={styles.container}>
-            <Box {...newsletter} isAnimated={true}/>
-
-            <div className={styles.email}>
-
-
-            </div>
-
+      <div className={styles.container}>
+        <Box {...newsletter} isAnimated={true} />
+        <div className={styles.email}>
+          <Text as="input" type="text" size="md"  />
         </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default memo(Newsletter);
--- src/componints/Nodos/Text.tsx
+++ src/componints/Nodos/Text.tsx
@@ -1,20 +1,30 @@
-import { memo } from "react";
+import React, { memo } from "react";
 
-type Props<E extends React.ElementType> = {
-  children: React.ReactNode;
-  size: "sm" | "md" | "lg";
+// 1. تعريف الخصائص بشكل منفصل
+type TextProps<E extends React.ElementType> = {
   as?: E;
-};
+  size: "sm" | "md" | "lg";
+  children?: React.ReactNode;
+} & Omit<React.ComponentPropsWithoutRef<E>, "as" | "size" | "children">;
 
-type TextProps<E extends React.ElementType> = Props<E> &
-  Omit<React.ComponentProps<E>, keyof Props<E>>;
+// 2. المكون كدالة عادية (بدون تغليف مباشر)
 function Text<E extends React.ElementType = "div">({
-  children,
-  size,
   as,
+  size,
+  children,
+  ...rest
 }: TextProps<E>) {
   const Component = as || "div";
-  return <Component className={`text-${size}`}>{children}</Component>;
+  return (
+    <Component className={`text-${size}`} {...rest}>
+      {children}
+    </Component>
+  );
 }
 
-export default memo(Text);
+/**
+ * هنا "الخدعة": نقوم بتصدير النسخة المغلفة بـ memo 
+ * ولكننا "نجبر" TypeScript على رؤيتها كدالة Generic 
+ * باستخدام Type Casting
+ */
+export default memo(Text) as typeof Text;
