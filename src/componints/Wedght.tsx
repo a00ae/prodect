dff76f4da722d311bd3c939680ba75e0d42a51ff
Hassan Alkhalaf <baraanas1521@gmail.com>
2026-05-01 19:54:40 +0300

    add new wedght

--- src/componints/Wedght.tsx
+++ src/componints/Wedght.tsx
@@ -1,25 +1,30 @@
 import "./scss/wedght.scss";
-import { memo } from "react";
+import { memo, useState } from "react"; // استيراد useState
 
-const arr = ["Studio", "Projects", "Blog", "Contact"];
 function Wedght() {
   console.log("Wedght");
+  const [isMenuOpen, setIsMenuOpen] = useState(false); // حالة للتحكم في فتح/إغلاق القائمة
+
+  const handleClickDropdown = () => {
+    setIsMenuOpen(!isMenuOpen); // تبديل حالة القائمة
+  };
   return (
-    <div className="wedght">
+    <div className={`wedght ${isMenuOpen ? "is-open" : ""}`} id="wedght">
+      {" "}
+      {/* تطبيق الفئة بناءً على الحالة */}
       <div className="container">
         <div className="image">
           <img src={`${import.meta.env.BASE_URL}Avater.svg`} alt="Avatar" />
         </div>
         <div className="nav">
           <ul className="nav-element">
-            {arr.map((name, index) => (
+            {["Studio", "Projects", "Blog", "Contact"].map((ele, index) => (
               <li key={index}>
-                <a className="top-text">{name}</a>
-                <a className="bottom-text">{name}</a>
+                <a ><p className="top-text" data-wedght={ele}>{ele}</p></a>
               </li>
             ))}
           </ul>
-          <div className="menu-nav">
+          <div onClick={handleClickDropdown} className="menu-nav">
             <div className="first"></div>
             <div className="last"></div>
           </div>
